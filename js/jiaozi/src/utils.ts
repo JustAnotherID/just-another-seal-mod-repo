1df74757edbf469e4c862fd4ea36b5781627bbe6
JustAnotherID <just-another-id@outlook.com>
2024-01-04 23:27:49 +0800

    feat(jiaozi): 增加饺子码v2，降低编码头长度

--- js/jiaozi/src/utils.ts
+++ js/jiaozi/src/utils.ts
@@ -1,6 +1,47 @@
+/*
+ * 饺子码编码格式：'饺'=0 '子'=1
+ * 版本号 3bit + 编码方式 1 bit
+ * 版本号 001 作为未控制包头的旧饺子码版本号
+ * 版本号 010 为当前版本（版本 2）
+ */
+
 import { gzip, ungzip } from 'pako';
 
 export function text2jiaozi(text: string): string {
+  return text2jiaoziV2(text);
+}
+
+/**
+ * 编码饺子码v1
+ *
+ * @param text 文本
+ * @returns 饺子码v1
+ */
+export function text2jiaoziV2(text: string): string {
+  const rawStr = stringToUtf8ByteArray(text)
+    .map((i) => i.toString(2).padStart(8, '0'))
+    .join('');
+  const compressed = Array.from(gzip(text, { level: 9 }) as Uint8Array)
+    .map((i) => i.toString(2).padStart(8, '0'))
+    .join('');
+  let bits;
+  if (compressed.length < rawStr.length) {
+    // 编码方式 1 为 gzip 压缩
+    const prefix = '0101';
+    bits = `${prefix}${compressed}`;
+  } else {
+    // 编码方式 0 为 utf8 原始编码
+    const prefix = '0100';
+    bits = `${prefix}${rawStr}`;
+  }
+  return bits2JiaoziChars(bits);
+}
+
+/**
+ * 未控制包头的旧饺子码编码
+ * @deprecated
+ */
+function oldText2jiaozi(text: string): string {
   const rawStr = stringToUtf8ByteArray(text)
     .map((i) => i.toString(2).padStart(8, '0'))
     .join('');
@@ -19,21 +60,58 @@ export function text2jiaozi(text: string): string {
       .join('');
     bits = `${prefix}${rawStr}`;
   }
-  return bits
-    .split('')
-    .map((bit) => {
-      if (bit === '0') {
-        return '饺';
-      } else if (bit === '1') {
-        return '子';
-      } else {
-        return '';
-      }
-    })
-    .join('');
+  return bits2JiaoziChars(bits);
 }
 
+/**
+ * 解析饺子码
+ */
 export function jiaozi2text(jiaozi: string): string | undefined {
+  const versionStr = jiaozi.slice(0, 3);
+  switch (versionStr) {
+    // 001: 旧饺子码
+    case '饺饺子':
+      return oldJiaozi2text(jiaozi);
+    // 010: 饺子码v2
+    case '饺子饺':
+      return jiaozi2textV2(jiaozi);
+    default:
+      return undefined;
+  }
+}
+
+/**
+ * 解析饺子码v2
+ */
+function jiaozi2textV2(jiaozi: string): string | undefined {
+  const format = jiaozi.slice(3, 4);
+  switch (format) {
+    // 编码方式 0 为 utf8 原始编码
+    case '饺':
+      return utf8ByteArrayToString(
+        jiaoziChars2bits(jiaozi.slice(4))
+          .match(/.{8}/g)
+          .map((i) => parseInt(i, 2))
+      );
+    // 编码方式 1 为 gzip 压缩
+    case '子':
+      const bits = new Uint8Array(
+        jiaoziChars2bits(jiaozi.slice(4))
+          .match(/.{8}/g)
+          .map((i) => parseInt(i, 2))
+      );
+      return ungzip(bits, { to: 'string' });
+    default:
+      return undefined;
+  }
+}
+
+/**
+ * 解析旧饺子码
+ * 格式为 版本号:编码格式:数据
+ * @deprecated
+ */
+function oldJiaozi2text(jiaozi: string): string | undefined {
   const bits = jiaozi
     .split('')
     .map((c) => {
@@ -140,4 +218,34 @@ function utf8ByteArrayToString(bytes: Uint8Array | Array<number>): string {
     }
   }
   return out.join('');
+}
+
+function bits2JiaoziChars(bits: string) {
+  return bits
+    .split('')
+    .map((bit) => {
+      if (bit === '0') {
+        return '饺';
+      } else if (bit === '1') {
+        return '子';
+      } else {
+        return '';
+      }
+    })
+    .join('');
+}
+
+function jiaoziChars2bits(jiaozi: string) {
+  return jiaozi
+    .split('')
+    .map((c) => {
+      if (c === '饺') {
+        return '0';
+      } else if (c === '子') {
+        return '1';
+      } else {
+        return '';
+      }
+    })
+    .join('');
 }
